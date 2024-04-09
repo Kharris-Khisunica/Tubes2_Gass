15878757623ed31b3e36b28786594430c41a20f5
Kharris-Khisunica <kharriskhisunica07@gmail.com>
2024-04-09 15:56:02 +0700

    add goquery

---
+++ src/graf.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"goquery"
+	"fmt"
+)
+
+func main() {
+	// Kamus Variabel
+	// var NamaVariabel type isi/kosong. -> bisa diassign nilai dibawah
+	// NamaVariabel := isi -> bisa
+	// const NamaKonst type isi -> Gk bisa diganti2
+	// const (Konst1 type1 Konst2 type2 ... )
+
+	//Gmn buat array
+
+	/*Algoritma
+	BFS = Ambil ID awal. Lalu cek anak dari ID tersebut. Misal di level 1 ada n anak jadi n kali. */
+
+	// Fungsi
+	fmt.Println("Hello World")
+
+}
